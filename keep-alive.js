3da14a344d1016028fa24d710eec6af59213d20c
sherkodi14 <sherkodi14@gmail.com>
2025-08-26 19:11:47 +0200

    Fix keep-alive script to use ES modules

--- keep-alive.js
+++ keep-alive.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
 // Load environment variables
-require('dotenv').config();
+dotenv.config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
